76c10979a9966069eb2cf4e0473f2dbe65aa781d
elbert nathanael <elbertwithane@gmail.com>
2025-01-20 18:02:55 +0800

    added logs and other funcs

--- main.js
+++ main.js
@@ -1,29 +1,38 @@
 // create main update loop
 function mainLoop() {
   // todo render events history
-  console.log(States.eventLogs);
-  const elEventLogs = document.getElementById("eventLogs");
-  elEventLogs.innerHTML = "";
-  States.eventLogs.forEach((event) => {
-    elEventLogs.innerHTML += `
-    <div>
-        <strong>event</strong>:${event.event}, <strong>payload</strong>: ${event.payload}
-    </div> 
-    `;
-  });
+  renderEventHistory;
   // todo render states
   // todo render convertsation history
 }
 setInterval(mainLoop, 1000 * 0.3);
 
+// rendering
+function renderEventHistory() {
+  const elEventLogs = document.getElementById("eventLogs");
+  elEventLogs.innerHTML = "";
+  States.eventLogs.forEach((event) => {
+    console.log;
+    elEventLogs.innerHTML += `
+    <div>
+        <strong>event</strong>:${
+          event.event
+        }, <strong>payload</strong>: ${event.payload.toString()}
+    </div> 
+    `;
+  });
+}
+
 // event callbacks
+let sockets = null;
 function onConnectButtonClicked() {
   // start the websocket
   const url = document.getElementById("socketUrlInput").value;
   const sockets = new Sockets(url);
   // add maessage callback
-  sockets.onMessageCallback = (data) => {
-    const dataParsed = JSON.parse(data);
-    States.eventLogs.push(dataParsed);
-  };
+  sockets.onMessageCallback = onEventReceived;
+}
+function onEventReceived(data) {
+  const dataParsed = JSON.parse(data);
+  States.eventLogs.push(dataParsed);
 }
